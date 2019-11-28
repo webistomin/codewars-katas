8f55b3c9259d466918b25e4447a397774f67c657
webistomin <webistomin@gmail.com>
2019-11-28 19:14:24 +0300

    feat(Sum of Cubes): solve Sum of Cubes

---
+++ 7 kyu/Sum of Cubes.js
@@ -0,0 +1,14 @@
+// Write a function that takes a positive integer n, sums all the cubed values from 1 to n, and returns that sum.
+//
+//   Assume that the input n will always be a positive integer.
+
+function sumCubes(n) {
+  let res = 0;
+  
+  for (let i = 1; i <= n; i++) {
+    res += i*i*i;
+  }
+  
+  return res;
+}
+
