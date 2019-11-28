a879e9155025e0786f7a1a70770b074d410f7084
webistomin <webistomin@gmail.com>
2019-11-28 19:14:55 +0300

    Sum of Cubes

--- 7 kyu/Sum of Cubes.js
+++ 7 kyu/Sum of Cubes.js
@@ -1,6 +1,6 @@
 // Write a function that takes a positive integer n, sums all the cubed values from 1 to n, and returns that sum.
 //
-//   Assume that the input n will always be a positive integer.
+// Assume that the input n will always be a positive integer.
 
 function sumCubes(n) {
   let res = 0;
